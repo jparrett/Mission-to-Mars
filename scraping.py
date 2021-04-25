b925df7e346006c40c6c42c95469e1382dfa3e3f
Jennifer Parrett <jennifer.parrett@comcast.net>
2021-04-25 09:30:14 -0400

    FinalCommit

--- scraping.py
+++ scraping.py
@@ -5,6 +5,8 @@ import pandas as pd
 import datetime as dt
 from webdriver_manager.chrome import ChromeDriverManager
 
+executable_path = {'executable_path': ChromeDriverManager().install()}
+browser = Browser('chrome', **executable_path)
 
 def scrape_all():
     # Initiate headless driver for deployment
@@ -19,7 +21,8 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
+        "last_modified": dt.datetime.now(),
+        "hemispheres": mars_images()
     }
 
     # Stop webdriver and return data
@@ -95,7 +98,30 @@ def mars_facts():
     df.set_index('Description', inplace=True)
 
     # Convert dataframe into HTML format, add bootstrap
-    return df.to_html(classes="table table-striped")
+    return df.to_html(classes="table table-hover")
+
+def mars_images():
+    # 1. Use browser to visit the URL 
+    url = 'https://data-class-mars-hemispheres.s3.amazonaws.com/Mars_Hemispheres/index.html'
+    browser.visit(url)
+
+    # 2. Create a list to hold the images and titles.
+    hemisphere_image_urls = []
+
+    # 3. Write code to retrieve the image urls and titles for each hemisphere.
+    html = browser.html
+    img_soup = soup(html, 'html.parser')
+
+    imgs=img_soup.find_all('div', class_='item')
+
+    for img in imgs:  
+        img_url_rel = img.find('img', class_='thumb').get('src')
+        img_url = f'https://data-class-mars-hemispheres.s3.amazonaws.com/Mars_Hemispheres/{img_url_rel}' 
+        title = img.find('h3').text
+        dictionary = {"img_url":img_url,"title": title}
+        hemisphere_image_urls.append(dictionary)
+
+    return hemisphere_image_urls
 
 if __name__ == "__main__":
 
